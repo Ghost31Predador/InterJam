eb9e48e484b8bb5c1068cc00b6c23463acbd6d63
Ghost31Predador <102180862+Ghost31Predador@users.noreply.github.com>
2024-03-05 09:29:53 -0500

    Arreglos importantes
    Se arreglo el bug que impedia ingresar al juego luego de presionar play

--- Assets/Martin Stuff/Scripts/MainMenu.cs
+++ Assets/Martin Stuff/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void CambiarEscena()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(2);
     }
     public void FuncionCerrarJuego()
     {
